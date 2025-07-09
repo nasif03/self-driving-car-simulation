07d2981b2f6114c2b551659199c528bb44036123
nasif03 <121179152+nasif03@users.noreply.github.com>
2025-07-09 19:44:41 +0600

    Update track

--- world.py
+++ world.py
@@ -6,13 +6,13 @@ from utils import blit_rotate_center
 
 # pygame setup
 FPS = 60
-SWIDTH = 1920
-SHEIGHT = 1080
+SWIDTH = 1280
+SHEIGHT = 720
 pygame.init()
 screen = pygame.display.set_mode((SWIDTH, SHEIGHT), vsync = 1)
 clock = pygame.time.Clock()
-background = pygame.image.load("assets/circle.png").convert_alpha()
-image_rect = background.get_rect(center=(SWIDTH // 2, SHEIGHT // 2))
+track = pygame.image.load("assets/track1.png").convert_alpha()
+track_rect = track.get_rect(center = (SWIDTH / 2, SHEIGHT / 2))
 
 def RotatePoint(point, origin, angle):
     rad = math.radians(angle)
@@ -36,8 +36,8 @@ class Polygon:
     def add_point(self, point):
         self.points.append(point)
     
-    def draw(self):
-        pygame.draw.polygon(screen, "BLACK", self.points, 4)
+    def draw(self, color):
+        pygame.draw.polygon(screen, color, self.points, 4)
 
     def rotate(self, angle):
         for point in self.points:
@@ -55,11 +55,13 @@ class Car:
     def __init__(self):
         self.poly = Polygon()
         self.center = pygame.Vector2(SWIDTH / 2, SHEIGHT / 2)
+        self.height = 20
+        self.width = 12
 
-        self.poly.add_point(pygame.Vector2(self.center.x - 60, self.center.y - 40))
-        self.poly.add_point(pygame.Vector2(self.center.x - 60, self.center.y + 40))
-        self.poly.add_point(pygame.Vector2(self.center.x + 60, self.center.y + 40))
-        self.poly.add_point(pygame.Vector2(self.center.x + 60, self.center.y - 40))
+        self.poly.add_point(pygame.Vector2(self.center.x - self.height / 2, self.center.y - self.width / 2))
+        self.poly.add_point(pygame.Vector2(self.center.x - self.height / 2, self.center.y + self.width / 2))
+        self.poly.add_point(pygame.Vector2(self.center.x + self.height / 2, self.center.y + self.width / 2))
+        self.poly.add_point(pygame.Vector2(self.center.x + self.height / 2, self.center.y - self.width / 2))
 
         self.velocity = 0;
         self.direction = 0;
@@ -70,22 +72,22 @@ class Car:
             x = int(x)
             y = int(y)
 
-            if image_rect.collidepoint(x, y):
-                x = x - image_rect.left
-                y = y - image_rect.top
-                pixel_color = background.get_at((x, y))
+            if track_rect.collidepoint(x, y):
+                x = x - track_rect.left
+                y = y - track_rect.top
+                pixel_color = track.get_at((x, y))
                 if pixel_color.a == 0:
                     return False
         return True
 
-    def draw(self):
-        self.poly.draw()
+    def draw(self, color):
+        self.poly.draw(color)
 
     def update(self):
         self.velocity = max(0, self.velocity - 0.1)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            self.velocity = min(self.velocity + 0.5, 7)
+            self.velocity = min(self.velocity + 0.5, 3.5)
         if keys[pygame.K_s]:
             self.velocity = max(self.velocity - 0.5, 0)
         if keys[pygame.K_a] and self.velocity > 0:
@@ -114,13 +116,13 @@ def main():
         car.update()
         
         # draw
-        if car.check_collision():
-            screen.fill("WHITE")
-        else:
-            screen.fill("RED")
-        screen.blit(background, image_rect)
+        screen.fill("WHITE")
+        screen.blit(track, track_rect)
 
-        car.poly.draw()
+        if car.check_collision():
+            car.poly.draw("BLUE")
+        else:
+            car.poly.draw("RED")
 
         pygame.display.flip()
 
